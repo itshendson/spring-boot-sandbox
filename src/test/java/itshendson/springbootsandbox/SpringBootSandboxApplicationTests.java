8d2ad37d771018c7372d86c88e4a0e1f82cf5530
Hendson <hendsonszeto@live.com>
2022-08-30 20:17:25 -0700

    Initialize spring boot sandbox project

---
+++ src/test/java/itshendson/springbootsandbox/SpringBootSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package itshendson.springbootsandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
