a789ded97c7d17450013278b47b7e9c3a55c394e
Hendson <hendsonszeto@live.com>
2022-09-01 11:03:02 -0700

    Implement slf4j logger for saveDepartment() and fetchDepartmentList()

--- src/main/java/itshendson/springbootsandbox/controller/DepartmentController.java
+++ src/main/java/itshendson/springbootsandbox/controller/DepartmentController.java
@@ -2,6 +2,8 @@ package itshendson.springbootsandbox.controller;
 
 import itshendson.springbootsandbox.entity.Department;
 import itshendson.springbootsandbox.service.DepartmentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
@@ -17,18 +19,25 @@ public class DepartmentController {
     private DepartmentService departmentService;
 
     /**
+     * This is the slf4j logger in-built to Spring. Another logger is logback
+     */
+    private final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
+
+    /**
      *
      * @param department the @RequestBody converts the json to Department object.
      * @return Department
      */
     @PostMapping("/departments")
     public Department saveDepartment(@Valid @RequestBody Department department) {
+        LOGGER.info("Log: saveDepartment in DepartmentController");
 //      DepartmentService service = new DepartmentServiceImpl(); You are using Spring, don't manually create new object! Spring should have control of creating new objects. Inversion of Control!!!
         return departmentService.saveDepartment(department);
     }
 
     @GetMapping("/departments")
     public List<Department> fetchDepartmentList() {
+        LOGGER.info("Log: fetchDepartmentList in DepartmentController");
         return departmentService.fetchDepartmentList();
     }
 
