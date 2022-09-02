95b2981320cecb7732f349e46286a720daf3734e
Hendson <hendsonszeto@live.com>
2022-09-02 09:56:06 -0700

    Switch from application .properties to .yml and created dev and prod profiles

--- src/main/java/itshendson/springbootsandbox/controller/RestController.java
+++ src/main/java/itshendson/springbootsandbox/controller/RestController.java
@@ -1,13 +1,18 @@
 package itshendson.springbootsandbox.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
 
+    // Pull **value** from application properties
+    @Value("${welcome.message}")
+    private String greeting;
+
     @GetMapping("/")
     public String helloWorld() {
-        return "<h1>Hello World!</h1>";
+        return greeting;
     }
 
     @GetMapping("/api")
